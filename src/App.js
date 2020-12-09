1a4fe862d51ad4188a784903b4e03bdee3aea3d1
İsa Kulaksız <isakulaksiz@outlook.com>
2020-12-09 11:54:25 +0300

    Add list of posts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import Search from './search';
 import './App.css';
 
+const posts =  [
+  {id: '1', name: "Clean code"},
+  {id: '2', name: "React design pattern and best practice"},
+  {id: '3', name: 'Gof design pattern'},
+  {id: '4', name: 'Artificial intelligence'}
+];
+
 const App = () => {
   return(
-    <Search />
+    <div>
+      <Search />
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
