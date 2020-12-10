43803f93482666aab2fc4339039857028961df4d
İsa Kulaksız <isakulaksiz@outlook.com>
2020-12-10 15:33:25 +0300

    Adding SPA navigation with router

--- src/search.js
+++ src/search.js
@@ -1,11 +1,21 @@
-const SearchBar = ({searchQuery, setSearchQuery}) => (
-    <form action="/" method="get">
-        <label htmlFor="heder-search">
-            <span className="visually-hidden">Search blog post</span>
-        </label>
+import {useHistory} from 'react-router-dom';
 
-        <input value={searchQuery} onInput={e => setSearchQuery(e.target.value)} type="text" id="header-search" placeholder="Search blog post" name="s" />
-        <button type="submit">Search</button>
-    </form>
-);
+const SearchBar = ({searchQuery, setSearchQuery}) => {
+    const history = useHistory();
+    const onSubmit = e => {
+        history.push(`?s=${searchQuery}`)
+        e.preventDefault();
+    }
+    return (
+        <form action="/" method="get" autoComplete="off" onSubmit={onSubmit}>
+            <label htmlFor="heder-search">
+                <span className="visually-hidden">Search blog post</span>
+            </label>
+
+            <input value={searchQuery} onInput={e => setSearchQuery(e.target.value)} type="text" id="header-search" placeholder="Search blog post" name="s" />
+            <button type="submit">Search</button>
+        </form>
+    );
+    
+};
 export default SearchBar;
